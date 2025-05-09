67c01ab76857f71f4f88a4e69bfae4527d191f22
seonghyeok <chosh2001@naver.com>
2025-05-09 17:24:00 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/org/ll/bugburgerbackend/global/baseEntity/BaseEntity.java
@@ -0,0 +1,40 @@
+package org.ll.bugburgerbackend.global.baseEntity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@MappedSuperclass
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+@Getter
+@Setter
+@SuperBuilder
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+@EqualsAndHashCode
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @CreatedDate
+    @Getter
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Getter
+    private LocalDateTime modifiedAt;
+}
