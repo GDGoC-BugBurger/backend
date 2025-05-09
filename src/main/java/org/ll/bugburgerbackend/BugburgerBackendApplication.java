ce54dfc2ff2b8dce69ab46ea56a7e9d29e89d83a
seonghyeok <chosh2001@naver.com>
2025-05-09 13:20:29 +0900

    init

---
+++ src/main/java/org/ll/bugburgerbackend/BugburgerBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ll.bugburgerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugburgerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugburgerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ll/bugburgerbackend/BugburgerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ll.bugburgerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugburgerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
